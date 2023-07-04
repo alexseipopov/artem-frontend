40d992b45b203d2e6c65d9de940efa7d5642e697
Alexsei Popov <popov2895@gmail.com>
2023-07-04 18:00:33 +0300

    add axios request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    useEffect(
-        () => {
-            axios.get("htth://localhost:5000/card")
-                .then(data => {
-                    console.log(data.data);
-                })
-        }
-    )
-  return (
-    <div className="App">
-        <h1>React 18 Alpha</h1>
-    </div>
-  );
+    const [cards, setCards] = useState([]);
+    useEffect(() => {
+        axios("http://127.0.0.1:5000/cards", {
+            method: "GET",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            }
+        })
+            .then(data => {
+                console.log(data.data);
+                setCards(data.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+    return (
+        <div className="App">
+            <h1>React 18 Alpha</h1>
+            {cards.map(card => (
+                <div className="card">
+                    <h2>{card.id}</h2>
+                    <p>{card.name}</p>
+                </div>
+            )  )}
+        </div>
+    );
 }
 
 export default App;
