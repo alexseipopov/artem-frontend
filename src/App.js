890a1d9e8d45beddf9520ac3b06495b6f0e5f61b
Alexsei Popov <popov2895@gmail.com>
2023-07-04 17:36:03 +0300

    add first data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect} from "react";
 
 function App() {
+    useEffect(
+        () => {
+            axios.get("htth://localhost:5000/card")
+                .then(data => {
+                    console.log(data.data);
+                })
+        }
+    )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>React 18 Alpha</h1>
     </div>
   );
 }
